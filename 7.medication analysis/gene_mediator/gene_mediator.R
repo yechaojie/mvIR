1adae5b6113f3e15b1c877ed11526339a57b01c9
Chaojie Ye <116529743+yechaojie@users.noreply.github.com>
2025-09-24 02:35:47 +0800

    Delete 7.medication analysis directory

--- 7.medication analysis/gene_mediator/gene_mediator.R
+++
@@ -1,4 +0,0 @@
-
-data <- readxl::read_xlsx(
-  "*/21gene_mediator_mydata.xlsx")
-res <- TwoSampleMR::mr(mydata)
--- 7.medication analysis/mediator_IR/mediator_IR.R
+++
@@ -1,4 +0,0 @@
-
-data <- readxl::read_xlsx(
-  "*/mediator_Fins_mydata.xlsx")
-res <- TwoSampleMR::mr(mydata)
